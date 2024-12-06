8a0100d8a1026b1ae5d8834f320580867af146d3
Grace Quigley <gquigley47@gmail.com>
2024-12-06 15:35:26 -0600

    getting my own data set up (kinda)

--- nextjs-dashboard/app/lib/placeholder-data.ts
+++ nextjs-dashboard/app/lib/placeholder-data.ts
@@ -1,147 +1,105 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
-const users = [
+const categories = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
-    password: '123456',
+    name: 'Electrical',
+    description: 'electrical tools'
+  },
+  {
+    id: '1d76b0dd-f800-46eb-8fa4-bf76352b919c',
+    name: 'Plumbing',
+    description: 'plumbing tools'
+  },
+  {
+    id: 'e1717e4e-583f-404e-aa41-5242e24e8a0d',
+    name: 'Paint',
+    description: 'painting supplies'
   },
 ];
 
-const customers = [
+const items = [
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
+    name: 'Black Vinyl Electrical Tape',
+    category_id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    location_id: 'da447b53-027d-42b0-b1ca-b057c7b1681f',
+    description: 'Scotch',
+    quantity: 5,
+    image_url: '/items/black-electrical-tape.png',
   },
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
-    image_url: '/customers/delba-de-oliveira.png',
+    name: 'Orange Wire Nuts',
+    category_id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    location_id: 'da447b53-027d-42b0-b1ca-b057c7b1681f',
+    description: "",
+    quantity: 25,
+    image_url: '/items/orange-wire-nuts.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
-    image_url: '/customers/lee-robinson.png',
+    category_id: '1d76b0dd-f800-46eb-8fa4-bf76352b919c',
+    name: 'Oatey Purple PVC Cement',
+    location_id: "a0342d48-d9ed-46e5-a8d7-6f7f98e6de69",
+    description: 'oatey brand purple bonding PVC cement',
+    quantity: 1,
+    image_url: '/items/oatey-pvc-purple-cement.png',
   },
   {
-    id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
-    email: 'michael@novotny.com',
-    image_url: '/customers/michael-novotny.png',
+    id: 'e1717e4e-583f-404e-aa41-5242e24e8a0d',
+    category_id: '4b7e7197-d2af-468d-9a82-8cc6153de4e4',
+    name: 'Floetrol',
+    location_id: "",
+    description: 'Floetrol for paint consistency',
+    quantity: 1,
+    image_url: '/items/floetrol.png',
   },
-  {
-    id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
-    name: 'Amy Burns',
-    email: 'amy@burns.com',
-    image_url: '/customers/amy-burns.png',
-  },
-  {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-    name: 'Balazs Orban',
-    email: 'balazs@orban.com',
-    image_url: '/customers/balazs-orban.png',
-  },
+  // {
+  //   id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
+  //   name: 'Amy Burns',
+  //   email: 'amy@burns.com',
+  //   image_url: '/customers/amy-burns.png',
+  // },
+  // {
+  //   id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+  //   name: 'Balazs Orban',
+  //   email: 'balazs@orban.com',
+  //   image_url: '/customers/balazs-orban.png',
+  // },
 ];
 
-const invoices = [
+const locations = [
   {
-    customer_id: customers[0].id,
-    amount: 15795,
-    status: 'pending',
-    date: '2022-12-06',
+    id: 'a0342d48-d9ed-46e5-a8d7-6f7f98e6de69',
+    name: 'Bin 1',
+    description: 'Storage Room Rack 2'
   },
   {
-    customer_id: customers[1].id,
-    amount: 20348,
-    status: 'pending',
-    date: '2022-11-14',
+    id: '4b7e7197-d2af-468d-9a82-8cc6153de4e4',
+    name: 'Bin 2',
+    description: 'Storage Room Rack 2'
   },
   {
-    customer_id: customers[4].id,
-    amount: 3040,
-    status: 'paid',
-    date: '2022-10-29',
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 44800,
-    status: 'paid',
-    date: '2023-09-10',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 34577,
-    status: 'pending',
-    date: '2023-08-05',
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 54246,
-    status: 'pending',
-    date: '2023-07-16',
-  },
-  {
-    customer_id: customers[0].id,
-    amount: 666,
-    status: 'pending',
-    date: '2023-06-27',
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 32545,
-    status: 'paid',
-    date: '2023-06-09',
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 1250,
-    status: 'paid',
-    date: '2023-06-17',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8546,
-    status: 'paid',
-    date: '2023-06-07',
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 500,
-    status: 'paid',
-    date: '2023-08-19',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8945,
-    status: 'paid',
-    date: '2023-06-03',
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 1000,
-    status: 'paid',
-    date: '2022-06-05',
-  },
+    id: 'da447b53-027d-42b0-b1ca-b057c7b1681f',
+    name: 'Bin 3',
+    description: 'Storage Room Rack 2'
+  }
 ];
 
-const revenue = [
-  { month: 'Jan', revenue: 2000 },
-  { month: 'Feb', revenue: 1800 },
-  { month: 'Mar', revenue: 2200 },
-  { month: 'Apr', revenue: 2500 },
-  { month: 'May', revenue: 2300 },
-  { month: 'Jun', revenue: 3200 },
-  { month: 'Jul', revenue: 3500 },
-  { month: 'Aug', revenue: 3700 },
-  { month: 'Sep', revenue: 2500 },
-  { month: 'Oct', revenue: 2800 },
-  { month: 'Nov', revenue: 3000 },
-  { month: 'Dec', revenue: 4800 },
-];
+// const revenue = [
+//   { month: 'Jan', revenue: 2000 },
+//   { month: 'Feb', revenue: 1800 },
+//   { month: 'Mar', revenue: 2200 },
+//   { month: 'Apr', revenue: 2500 },
+//   { month: 'May', revenue: 2300 },
+//   { month: 'Jun', revenue: 3200 },
+//   { month: 'Jul', revenue: 3500 },
+//   { month: 'Aug', revenue: 3700 },
+//   { month: 'Sep', revenue: 2500 },
+//   { month: 'Oct', revenue: 2800 },
+//   { month: 'Nov', revenue: 3000 },
+//   { month: 'Dec', revenue: 4800 },
+// ];
 
-export { users, customers, invoices, revenue };
+export { categories, items, locations };
--- nextjs-dashboard/app/seed/route.ts
+++ nextjs-dashboard/app/seed/route.ts
@@ -1,122 +1,117 @@
-// import bcrypt from 'bcrypt';
-// import { db } from '@vercel/postgres';
-// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import bcrypt from 'bcrypt';
+import { db } from '@vercel/postgres';
+import { locations, items, categories } from '../lib/placeholder-data';
 
-// const client = await db.connect();
+const client = await db.connect();
 
-// async function seedUsers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS users (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email TEXT NOT NULL UNIQUE,
-//       password TEXT NOT NULL
-//     );
-//   `;
+// async function seedCategories() {
+  // await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  // await client.sql`
+  //   CREATE TABLE IF NOT EXISTS categories (
+  //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  //     name VARCHAR(255) NOT NULL,
+  //     description TEXT,
+  //   );
+  // `;
 
-//   const insertedUsers = await Promise.all(
-//     users.map(async (user) => {
-//       const hashedPassword = await bcrypt.hash(user.password, 10);
+//   const insertedCategories = await Promise.all(
+//     categories.map(async (category) => {
 //       return client.sql`
-//         INSERT INTO users (id, name, email, password)
-//         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+//         INSERT INTO categories (id, name, description)
+//         VALUES (${category.id}, ${category.name}, ${category.description})
 //         ON CONFLICT (id) DO NOTHING;
 //       `;
 //     }),
 //   );
 
-//   return insertedUsers;
+//   return insertedCategories;
 // }
 
-// async function seedInvoices() {
+// async function seedItems() {
 //   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
 //   await client.sql`
-//     CREATE TABLE IF NOT EXISTS invoices (
+//     CREATE TABLE IF NOT EXISTS items (
 //       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       customer_id UUID NOT NULL,
-//       amount INT NOT NULL,
-//       status VARCHAR(255) NOT NULL,
-//       date DATE NOT NULL
+//       location_id UUID NOT NULL,
+//       category_id UUID NOT NULL,
+//       description TEXT, 
+//       quantity INT,
+//       image_url VARCHAR(255)
 //     );
 //   `;
 
-//   const insertedInvoices = await Promise.all(
-//     invoices.map(
-//       (invoice) => client.sql`
-//         INSERT INTO invoices (customer_id, amount, status, date)
-//         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+//   const insertedItems = await Promise.all(
+//     items.map(
+//       (item) => client.sql`
+//         INSERT INTO invoices (id, location_id, category_id, description, quantity, image_url)
+//         VALUES (${item.id}, ${item.location_id}, ${item.category_id}, 
+//           ${item.description}, ${item.quantity}, ${item.image_url})
 //         ON CONFLICT (id) DO NOTHING;
 //       `,
 //     ),
 //   );
 
-//   return insertedInvoices;
+//   return insertedItems;
 // }
 
-// async function seedCustomers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+async function seedLocations() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS customers (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email VARCHAR(255) NOT NULL,
-//       image_url VARCHAR(255) NOT NULL
-//     );
-//   `;
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS locations (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      description TEXT
+    );
+  `;
 
-//   const insertedCustomers = await Promise.all(
-//     customers.map(
-//       (customer) => client.sql`
-//         INSERT INTO customers (id, name, email, image_url)
-//         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+  const insertedLocations = await Promise.all(
+    locations.map(
+      (location) => client.sql`
+        INSERT INTO locations (id, name, description)
+        VALUES (${location.id}, ${location.name}, ${location.description}})
+        ON CONFLICT (id) DO NOTHING;
+      `,
+    ),
+  );
 
-//   return insertedCustomers;
-// }
+  return insertedLocations;
+}
 
-// async function seedRevenue() {
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS revenue (
-//       month VARCHAR(4) NOT NULL UNIQUE,
-//       revenue INT NOT NULL
-//     );
-//   `;
+// // async function seedRevenue() {
+// //   await client.sql`
+// //     CREATE TABLE IF NOT EXISTS revenue (
+// //       month VARCHAR(4) NOT NULL UNIQUE,
+// //       revenue INT NOT NULL
+// //     );
+// //   `;
 
-//   const insertedRevenue = await Promise.all(
-//     revenue.map(
-//       (rev) => client.sql`
-//         INSERT INTO revenue (month, revenue)
-//         VALUES (${rev.month}, ${rev.revenue})
-//         ON CONFLICT (month) DO NOTHING;
-//       `,
-//     ),
-//   );
+// //   const insertedRevenue = await Promise.all(
+// //     revenue.map(
+// //       (rev) => client.sql`
+// //         INSERT INTO revenue (month, revenue)
+// //         VALUES (${rev.month}, ${rev.revenue})
+// //         ON CONFLICT (month) DO NOTHING;
+// //       `,
+// //     ),
+// //   );
 
-//   return insertedRevenue;
-// }
+//   // return insertedRevenue;
+// // }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  //   await client.sql`BEGIN`;
-  //   await seedUsers();
-  //   await seedCustomers();
-  //   await seedInvoices();
-  //   await seedRevenue();
-  //   await client.sql`COMMIT`;
+  try {
+    await client.sql`BEGIN`;
+    // await seedCategories();
+    await seedLocations();
+    // await seedItems();
+    // await seedRevenue();
+    await client.sql`COMMIT`;
 
-  //   return Response.json({ message: 'Database seeded successfully' });
-  // } catch (error) {
-  //   await client.sql`ROLLBACK`;
-  //   return Response.json({ error }, { status: 500 });
-  // }
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    await client.sql`ROLLBACK`;
+    return Response.json({ error }, { status: 500 });
+  }
 }
